b80a961ebcb11a4c96f139aec3a6b3ad5f32ee0c
glever <glen.vermeylen@gmail.com>
2019-12-14 08:53:44 +0100

    initial commit from spring initializr

---
+++ src/test/java/be/glever/workoutifier/WorkoutifierApplicationTests.java
@@ -0,0 +1,13 @@
+package be.glever.workoutifier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkoutifierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
